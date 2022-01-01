40a9247f590c2a01aed461bfc34fd7e0fb34ddc0
MasahiroSato <k27c8ff627uxz@yahoo.co.jp>
2022-01-01 21:21:53 +0900

    Add eslint

---
+++ web/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  'parser': '@typescript-eslint/parser', 
+  'extends': [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  'rules': {
+    'semi': [ 'error', 'always' ],
+    'comma-dangle': [ 'error', 'always-multiline' ],
+    'quotes': ['error', 'single'],
+    'indent': [ 'error', 2 ],
+    'linebreak-style': [ 2, 'unix' ],
+    'eol-last': ['error', 'always'],
+    '@typescript-eslint/no-var-requires': 0,
+  },
+  'settings': {
+    'react': {
+      'version': 'detect',
+    },
+  },
+};
