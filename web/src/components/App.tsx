5798f9d1681052789c5048d56c9b9e552beec926
MasahiroSato <k27c8ff627uxz@yahoo.co.jp>
2022-01-16 23:16:30 +0900

    Add Firebase Initializing

--- web/src/components/App.tsx
+++ web/src/components/App.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
 import MainFramework from './MainFrameworks/MainFramework';
 import { HashRouter } from 'react-router-dom';
+import * as firebase from 'firebase/app';
+import InitializingError from './InitializingError';
 
 const App: React.FC = () => {
+
+  try {
+    firebase.initializeApp({
+      apiKey: process.env.REACT_APP_API_KEY,
+      authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+      projectId: process.env.REACT_APP_PROJECT_ID,
+      storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+      messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+      appId: process.env.REACT_APP_APP_ID,
+    });
+  } catch(e) {
+    console.log(e);
+    return (<InitializingError />);
+  }
+
   return (
     <HashRouter>
       <MainFramework drawerWidth={240}/>
---
+++ web/src/components/InitializingError.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+
+const InitializingError: React.FC = () => {
+  return (
+    <React.Fragment>
+      <Typography>
+        Initializing Error Occurs.
+      </Typography>
+    </React.Fragment>
+  );
+};
+
+export default InitializingError;
