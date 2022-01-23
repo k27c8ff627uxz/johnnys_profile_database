50ac44ffe3b3999a46b6df9ff1363c41a6ec808b
MasahiroSato <k27c8ff627uxz@yahoo.co.jp>
2022-01-24 02:15:18 +0900

    eslint (in function)

---
+++ functions/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  'parser': '@typescript-eslint/parser',
+  'extends': [
+    'plugin:@typescript-eslint/recommended',
+  ],
+  'rules': {
+    'semi': [ 'error', 'always' ],
+    'comma-dangle': [ 'error', 'always-multiline' ],
+    'quotes': ['error', 'single'],
+    'indent': [ 'error', 2 ],
+    'linebreak-style': [ 2, 'unix' ],
+    'eol-last': ['error', 'always'],
+    '@typescript-eslint/no-var-requires': 0,
+  },
+};
