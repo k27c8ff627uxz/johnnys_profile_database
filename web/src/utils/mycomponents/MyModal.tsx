9d03e2a0fefcd05dad14c4904c0c5d4c72493d69
MasahiroSato <k27c8ff627uxz@yahoo.co.jp>
2022-02-10 00:27:59 +0900

    Add 'MyModal'

---
+++ web/src/utils/mycomponents/MyModal.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import {
+  AppBar,
+  Box,
+  IconButton,
+  LinearProgress,
+  Modal,
+  Paper,
+  Toolbar,
+} from '@mui/material';
+import { Close as CloseIcon } from '@mui/icons-material';
+
+export interface MyModalProp {
+  children: React.ReactNode;
+  open: boolean;
+  onClose: () => void;
+  title?: string;
+  isLoading: boolean;
+}
+
+const MyModal = (props: MyModalProp) => {
+  const { open, onClose, children, isLoading, title } = props;
+
+  const style: React.CSSProperties  = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+  };
+
+  return (
+    <Modal
+      open={open}
+      onClose={onClose}
+    >
+      <Box style={style}>
+        <Paper>
+          {title && <>
+            <AppBar>
+              <Toolbar variant='dense'>
+                <IconButton edge='start' onClick={onClose} sx={{padding: 0, mr: 1}} >
+                  <CloseIcon fontSize='inherit' />
+                </IconButton>
+                {title}
+              </Toolbar>
+            </AppBar>
+            <Toolbar variant='dense' />
+            {isLoading && <LinearProgress />}
+          </>}
+          {children}
+        </Paper>
+      </Box>
+    </Modal>
+  );
+};
+
+export default MyModal;
--- web/src/utils/mycomponents/index.ts
+++ web/src/utils/mycomponents/index.ts
@@ -1,8 +1,10 @@
 import MyErrorMessage from './MyErrorMessage';
 import { MySuccessSnackbar, MyErrorSnackbar } from './MySnackbar';
+import MyModal from './MyModal';
 
 export {
   MyErrorMessage,
   MySuccessSnackbar,
   MyErrorSnackbar,
+  MyModal,
 };
