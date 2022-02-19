32f6572248dc3ff2872b1145394a3d4f41f47c9e
MasahiroSato <k27c8ff627uxz@yahoo.co.jp>
2022-02-19 18:47:32 +0900

    Add Register Button in Add Profile

--- web/src/components/pages/ProfileList/AddUserEditor.tsx
+++ web/src/components/pages/ProfileList/AddUserEditor.tsx
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import { Box, Stack } from '@mui/material';
+import FrameworkViewContainer from 'models/frameworkView';
+import { ButtonWithProgress } from 'utils/mycomponents';
 import ProfileEditor, { ProfileEditorValue } from './ProfileEditor';
 
 const initialValue: ProfileEditorValue = {
@@ -19,8 +22,41 @@ const initialValue: ProfileEditorValue = {
 
 const AddUserEditor = () => {
   const [value, setValue] = useState(initialValue);
+  const { isLoading, beginLoading, finishLoading } = FrameworkViewContainer.useContainer();
+
+  const verify = (() => {
+    if (!value.name || !value.furigana || !value.dateOfBirth || !value.bloodType) {
+      return true;
+    }
+
+    if (isLoading) {
+      return true;
+    }
+
+    return false;
+  })();
+
+  const onSubmit = () => {
+    beginLoading();
+    console.log('Submit');
+    finishLoading();
+  };
+
   return (
-    <ProfileEditor value={value} onChange={v => setValue(v)} sx={{p: 4}} />
+    <Stack spacing={3} sx={{m: 3}}>
+      <ProfileEditor value={value} onChange={v => setValue(v)} />
+      <Box sx={{display: 'flex', justifyContent: 'flex-end'}}>
+        <ButtonWithProgress
+          type='submit'
+          variant='contained'
+          disabled={verify}
+          isLoading={isLoading}
+          onClick={() => onSubmit()}
+        >
+          登録
+        </ButtonWithProgress>
+      </Box>
+    </Stack>
   );
 };
 
