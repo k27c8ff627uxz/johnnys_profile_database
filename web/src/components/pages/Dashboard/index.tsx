7b8cfdbed87364bb96e43cc1126da34a0de0255a
MasahiroSato <k27c8ff627uxz@yahoo.co.jp>
2022-03-18 23:07:59 +0900

    Add Dashboard

---
+++ web/src/components/pages/Dashboard/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+  Box,
+  Stack,
+} from '@mui/material';
+import TodayNews from './todayNews';
+
+const Dashboard: React.FC = () => {
+  return (
+    <Box style={{display: 'flex', justifyContent: 'center'}}>
+      <Stack sx={{margin: 3, width: '80%'}} spacing={1}>
+        <TodayNews />
+      </Stack>
+    </Box>
+  );
+};
+
+export default Dashboard;
---
+++ web/src/components/pages/Dashboard/todayNews/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+  Typography,
+} from '@mui/material';
+
+const TodayNews = () => {
+  return (
+    <React.Fragment>
+      <Typography variant='h6'>
+        今日のお知らせ
+      </Typography>
+    </React.Fragment>
+  );
+};
+
+export default TodayNews;
